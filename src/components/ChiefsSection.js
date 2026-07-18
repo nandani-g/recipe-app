bc6adf77b282c5e1d10b32819f24da48671c0d9f
nandani-g <nandaniigupta02@gmail.com>
2026-07-18 23:53:58 +0530

    Update chief section

--- src/components/ChiefsSection.js
+++ src/components/ChiefsSection.js
@@ -1,9 +1,55 @@
+import ChiefCart from "./ChiefCard"
+
+
 export default function ChiefsSection(){
+  const chiefs = [
+    {
+      name:"Juan Carlos",
+      img: "/img/top-chiefs/img_1.jpg",
+      recipesCount:"10",
+      cuisine: "Mexican",
+    },
+    {
+      name: "John Doe",
+      img: "/img/top-chiefs/img_2.jpg",
+      recipesCount: "05",
+      cuisine: "Japanese"
+    }, {
+      name: "Erich Maria",
+      img: "/img/top-chiefs/img_3.jpg",
+      recipesCount: "13",
+      cuisine: "Italian"
+    },
+    {
+      name: "Chris Brown",
+      img: "/img/top-chiefs/img_4.jpg",
+      recipesCount: "08",
+      cuisine: "French"
+
+    }, {
+      name: "Blake Lively",
+      img: "/img/top-chiefs/img_5.jpg",
+      recipesCount: "09",
+      cuisine: "American"
+    },
+    {
+      name: "Ben Affleck",
+      img: "/img/top-chiefs/img_6.jpg",
+      recipesCount: "04",
+      cuisine: "Indian"
+    }
+
+  ]
+
   return(
     <div className="section chiefs">
       <h1 className="title">Our Top Chiefs</h1>
       <div className="top-chiefs-container">
-        
+        {chiefs.map(chief => <ChiefCart key = {chief.name} chief={chief} /> )}
+
+
+
+
       </div>
     </div>
   )
