24bf9de3af0720030c51bb56660cbe8363050be8
nandani-g <nandaniigupta02@gmail.com>
2026-07-21 21:14:38 +0530

    feat: update home page sections and animations

--- src/components/ChiefCard.js
+++ src/components/ChiefCard.js
@@ -1,25 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
-import {faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons"
-export default function ChiefCart({chief}){
-  return (
-    <div className="chief-card">
-      <img src={chief.img} alt={ChiefCart.name}/>
-      <div className="chief-card-info">
-        <h3 className="chief-card-name">{chief.name}</h3>
-        <p className="chief-recipe-count">Recipes: <b>{chief.recipesCount}</b></p>
-        <p className="chief-cuisine">cuisine: <b>{chief.cuisine}</b></p>
-
-        <p className="chief-icon">
-         
-          <FontAwesomeIcon icon = {faFacebook}/>
-           <FontAwesomeIcon icon = {faTwitter}/>
-            <FontAwesomeIcon icon = {faInstagram}/>
+import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 
-
-        </p>
-      </div>
-    </div>
-  )
+export default function ChiefCard({chief}) {
+    return (
+        <div className="chief-card">
+            <img src={chief.img} alt="" />
+            <div className="chief-card-info">
+                <h3 className="chief-card-name">{chief.name}</h3>
+                <p className="chief-recipe-count">Recipes: <b>{chief.recipesCount}</b></p>
+                <p className="chief-cuisine">Cuisine: <b>{chief.cuisine}</b></p>
+                <p className="cheif-icons">
+                    <FontAwesomeIcon icon={faFacebook} />
+                    <FontAwesomeIcon icon={faTwitter} />
+                    <FontAwesomeIcon icon={faInstagram} />
+                </p>
+            </div>
+        </div>
+    )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,16 @@
-
-import HeroSection from '../components/HeroSection';
-import ImproveSkills from '../components/ImproveSkills';
-import QuoteSection from '../components/QuoteSection';
-import ChiefsSection from '../components/ChiefsSection';
+import HeroSection from "../components/HeroSection";
+import ImproveSkills from "../components/ImproveSkills";
+import QouteSection from "../components/QuoteSection";
+import ChiefsSection from "../components/ChiefsSection";
 
 
 export default function Home(){
-  return (
-     <div>
-            <HeroSection/>
-            <ImproveSkills/>
-            <QuoteSection/>
-            <ChiefsSection/>
-     </div>
-  )
+    return (
+        <div>
+            <HeroSection />
+            <ImproveSkills />
+            <QouteSection />
+            <ChiefsSection />
+        </div>
+    )
 }
